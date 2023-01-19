18d482a315f04fd2bea6d48228488ab7053aedcd
KishorDMane <manekishor212@gmail.com>
2023-01-19 10:10:27 +0530

    show screen

--- backend-/index.js
+++ backend-/index.js
@@ -1,13 +1,16 @@
+const { Socket } = require("dgram");
+const {Server}=require("socket.io");
 const express=require("express");
-const Socket=require("socket.io")
 const http=require("http")
 const app=express();
 const serverHttp=http.createServer(app)
-const io=Socket(http)
-
+const io=new Server(serverHttp) 
 
 app.get("/shearedscreen",(req,res)=>{
     res.sendFile(__dirname+`/shearedscreen.html`)
+})
+app.get("/",(req,res)=>{
+    res.send("hello")
 })
 io.on("connection",(socket)=>{
     socket.on("join-member",(ID)=>{
@@ -19,9 +22,8 @@ io.on("connection",(socket)=>{
         const room=data.room;
         const imgSrc=data.image;
         socket.brodcast.to(room).emit("sheared-screen",imgSrc);
-
-
     })
+   
 })
 serverHttp.listen(8000,()=>{
     console.log("listening on 8000 port");
