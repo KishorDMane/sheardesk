2553333948e8ce860a7a96f9fe5cf7aa424f6e73
Kishor Mane <manekishor212@gmail.com>
2023-02-11 17:19:04 +0530

    downloade link

--- website/index.js
+++ website/index.js
@@ -2,3 +2,9 @@
 document.getElementById("sheared-screen").addEventListener("click",()=>{
     window.location.href="https://screen-shear-app.onrender.com/shearedscreen"
 })
+
+
+
+document.getElementById("winBtn").addEventListener("click",()=>{
+    window.location.href="https://drive.google.com/u/0/uc?id=14fBT-0FI3gv9xU1Wtwzdpk4ioI0RN4h4&export=download"
+})
